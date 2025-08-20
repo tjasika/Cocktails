446e44d7386be751d5a62dc06c6be33d3b6cd66b
Tjaša <tjasazzeleznik@gmail.com>
2025-08-20 21:33:06 +0200

    Adding category buttons
    To be later used for filtering

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './style.css'
 import cocktails from './data/data.json'
+import filters from './data/filters.json'
 import { CardsContainer } from './components/CardsContainer'
+import { FilterButton } from './components/FilterButton'
 
 function App() {
   return (
     <>
       <div className="flex flex-col justify-center items-center gap-5 p-5 w-full h-full bg-zinc-200">
-          <h1 className="text-3xl font-bold text-zinc-900">Cocktails</h1>
+          <h1 className="text-5xl font-bold text-zinc-700 pt-5">Cocktails</h1>
+          <div className='pb-5 pl-10 flex flex-row gap-3 items-left w-full'>
+              {filters.map((filter, index)=> (
+                <FilterButton key={index} text={filter.name} />
+              ))}
+          </div>
           <CardsContainer cocktails={cocktails}/>
       </div>
     </>
---
+++ src/components/FilterButton.jsx
@@ -0,0 +1,7 @@
+export const FilterButton = ({text}) => {
+    return (
+        <button className="p-2 rounded-3xl bg-zinc-50 w-45">
+            {text}
+        </button>
+    )
+}
