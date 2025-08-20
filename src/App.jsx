8c579e7a6cba7d7339f83ec632d5b550b43c60f1
Tjaša <tjasazzeleznik@gmail.com>
2025-08-20 21:38:34 +0200

    Buttons styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './style.css'
 import cocktails from './data/data.json'
-import filters from './data/filters.json'
+import categories from './data/categories.json'
 import { CardsContainer } from './components/CardsContainer'
 import { FilterButton } from './components/FilterButton'
 
@@ -9,8 +9,8 @@ function App() {
     <>
       <div className="flex flex-col justify-center items-center gap-5 p-5 w-full h-full bg-zinc-200">
           <h1 className="text-5xl font-bold text-zinc-700 pt-5">Cocktails</h1>
-          <div className='pb-5 pl-10 flex flex-row gap-3 items-left w-full'>
-              {filters.map((filter, index)=> (
+          <div className='pb-2 pl-10 flex flex-row gap-3 items-left w-full'>
+              {categories.map((filter, index)=> (
                 <FilterButton key={index} text={filter.name} />
               ))}
           </div>
--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,6 +1,6 @@
 export const FilterButton = ({text}) => {
     return (
-        <button className="p-2 rounded-3xl bg-zinc-50 w-45">
+        <button className="p-2 rounded-3xl bg-zinc-50 w-45 transition-colors duration-300 hover:bg-zinc-300 cursor-pointer">
             {text}
         </button>
     )
